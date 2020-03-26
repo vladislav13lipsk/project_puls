8c2b3150201c221d3646fd503f30d076685cf68f
Vlad <example@gmail.com>
2020-03-27 01:33:09 +0300

    mod

--- gulpfile.js
+++ gulpfile.js
@@ -18,13 +18,9 @@ gulp.task('server', function() {
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(gulp.dest('./css'))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer({
-            browers: ['last 2 versions'],
-            cascade: false
-        }))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
