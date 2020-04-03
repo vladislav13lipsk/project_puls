75eb9cecd6fdb92e5504ef8e1ec8fd307ba295aa
Vlad <example@gmail.com>
2020-04-03 19:22:04 +0300

    add carousel

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src ="icon/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src ="icon/arrow_right.png"></button>'
+      });
+  });
