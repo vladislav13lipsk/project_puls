55df9795f4a6be9e216fe9dd2972632cf9a3a3f9
Vlad <example@gmail.com>
2020-04-09 00:44:15 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src ="icon/arrow_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src ="icon/arrow_right.png"></button>'
       });
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
   });
